5882e39b0e79d5e0cccf1dcbf25bd8575726b76b
ryu93885 <arrownz1617@gmail.com>
2025-03-24 02:35:27 +0900

    app.py

--- app.py
+++ app.py
@@ -594,7 +594,6 @@ def main():
                     elif isinstance(file_data, str):
                         st.warning(file_data)
                     st.session_state.update({
-                        'radio_value':None
                     })
                     st.session_state.radio_value = None
             with col2:
@@ -605,7 +604,7 @@ def main():
                         del st.session_state.analysis_result
                     st.session_state.radio_value = None
                     st.session_state.update({
-                        'radio_value':None
+                        
                     })
                     st.experimental_rerun()
 
@@ -620,7 +619,7 @@ def main():
                         del st.session_state.analysis_result
                     st.session_state.radio_value = None
                     st.session_state.update({
-                        'radio_value':None
+                        
                     })
                     st.experimental_rerun()
 if __name__ == "__main__":
